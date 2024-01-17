13079fe398a1655bd97c0185a0bc713b2ba904e3
JoannaAdermark1 Adermark <joanna.adermark@tutanota.com>
2024-01-17 06:27:58 +0000

    changes in view and urls.py

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import TaskLisk
 
 urlpatterns = [
-    path('', views.tasklist, name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
 ]
