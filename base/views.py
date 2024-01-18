be3500d27d5c972133a40ed6e62ecb12ad9ebfda
JoannaAdermark1 Adermark <joanna.adermark@tutanota.com>
2024-01-18 17:01:43 +0000

    view task

--- base/views.py
+++ base/views.py
@@ -8,8 +8,9 @@ from .models import Task
 
 class TaskList(ListView):
       model = Task
-      context_object_name ='tasks'
-
+      context_object_name = 'tasks'
 
 class TaskDetail(DetailView):
-    model = Task      
+      model = Task
+      context_object_name = 'tasks'
+      template_name = 'base/task.html'
