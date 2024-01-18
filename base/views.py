5926f8d246914f42b3333e13363697cb6f04df4d
JoannaAdermark1 Adermark <joanna.adermark@tutanota.com>
2024-01-18 21:43:32 +0000

    create item

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 from .models import Task
 
 
@@ -14,3 +17,10 @@ class TaskDetail(DetailView):
       model = Task
       context_object_name = 'tasks'
       template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+      model = Task
+      field = '__all__'
+      success_url = reverse_lazy('tasks')
+
+
