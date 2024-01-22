67197418f506b724559c6815689930d961ac34d7
JoannaAdermark1 Adermark <joanna.adermark@tutanota.com>
2024-01-22 16:48:04 +0000

    allowed_hosts url

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(b0zvlkvx7-d$13vvut8v0r$!q2ngxs@%bo4lzd0-e4*7=40qg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-joannaaderm-mydjangopro-n7w2f9yd82t.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['myfirstdjangoproject-3acb455ce907.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://8000-joannaaderm-mydjangopro-n7w2f9yd82t.ws-eu107.gitpod.io']
 
 
